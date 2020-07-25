f9ab96e167e1504733e25816639936a13c1ab670
CalebBap <44696060+CalebBap@users.noreply.github.com>
2020-07-26 10:57:54 +1200

    Implements ability to request and handle data from OpenWeatherMap API

--- src/Components/WeatherGrid/WeatherGrid.tsx
+++ src/Components/WeatherGrid/WeatherGrid.tsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './WeatherGrid.css';
-import { TextField } from '@material-ui/core';
 
 interface IWeatherGridProps {
     SearchQuery: (string | null);
 }
 
+interface IWeather {
+    weather: string;
+    description: string;
+    temperature: number;
+    humidity: number;
+}
+
+let result: IWeather = {weather: "", description: "", temperature: 0, humidity: 0}; 
+
 function WeatherGrid(props: IWeatherGridProps) {
-    console.log(props.SearchQuery);
+    
+    useEffect( () => { 
+        fetch('http://api.openweathermap.org/data/2.5/weather?q=London&appid=' + process.env.REACT_APP_API_KEY)
+        .then(response => response.json())
+            .then(response => {
+                result = {  weather: response.weather[0].main, 
+                            description: response.weather[0].description, 
+                            temperature: response.main.temp, 
+                            humidity: response.main.humidity };
+            })
+        .catch(() => console.log("Fetch response error"));
+    });
+
     return (
-        <p>{props.SearchQuery}</p>
-    )
+        <p>{result.description}</p>
+    );
 }
 
 export default WeatherGrid;
